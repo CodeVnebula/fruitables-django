67a3fadb00b6b1ee4b048fb17ca2478e14351c0b
CodeVnebula <chkhikvadzem471@gmail.com>
2024-11-03 23:05:34 +0400

    Add handler404 and handler500 custom 404 and 500 views

--- fruitables/pages/urls.py
+++ fruitables/pages/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home_view'),
     path('about/', views.about_view, name='about_view'),
-    path('contact/', views.contact_view, name='contact_view'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
     path('testimonial/', views.testimonial_view, name='testimonial_view'),
 ]
+
+handler404 = views.Custom404View
+handler500 = views.Custom500View
